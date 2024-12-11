ff6035dea081665ff4cb9264b22152d292805786
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2024-12-11 15:39:23 +0700

    Update layout.jsx

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -7,14 +7,14 @@ import { Budgets } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
 import { useRouter } from "next/navigation";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 function DashboardLayout({ children }) {
   const { user } = useUser();
   const router = useRouter();
+
   useEffect(() => {
-    user && checkUserBudgets();
+    if (user) checkUserBudgets();
   }, [user]);
 
   const checkUserBudgets = async () => {
@@ -23,21 +23,25 @@ function DashboardLayout({ children }) {
       .from(Budgets)
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
     console.log(result);
-    if (result?.length == 0) {
+    if (result?.length === 0) {
       router.replace("/dashboard/budgets");
     }
   };
+
   return (
-    <Analytics/>
-    <div>
-      <div className="fixed md:w-64 hidden md:block ">
-        <SideNav />
+    <>
+      {/* Include Analytics at the top level of the layout */}
+      <Analytics />
+      <div>
+        <div className="fixed md:w-64 hidden md:block">
+          <SideNav />
+        </div>
+        <div className="md:ml-64">
+          <DashboardHeader />
+          {children}
+        </div>
       </div>
-      <div className="md:ml-64 ">
-        <DashboardHeader />
-        {children}
-      </div>
-    </div>
+    </>
   );
 }
 
