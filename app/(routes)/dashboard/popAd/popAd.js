780042d33affdeb37f9a22102ceb6559f98c83bb
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2025-05-07 21:09:58 +0700

    Update popAd.js

--- app/(routes)/dashboard/popAd/popAd.js
+++ app/(routes)/dashboard/popAd/popAd.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
-const PopAd = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//pl25520742.effectiveratecpm.com/1d/37/a4/1d37a47df5c490cd61796933d85a11f7.js';
-    script.async = true;
+// const PopAd = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = '//pl25520742.effectiveratecpm.com/1d/37/a4/1d37a47df5c490cd61796933d85a11f7.js';
+//     script.async = true;
 
-    script.onload = () => console.log("Script iklan berhasil dimuat.");
-    script.onerror = () => console.error("Gagal memuat skrip iklan.");
+//     script.onload = () => console.log("Script iklan berhasil dimuat.");
+//     script.onerror = () => console.error("Gagal memuat skrip iklan.");
 
-    document.body.appendChild(script);
+//     document.body.appendChild(script);
 
-    return () => {
-      if (document.body.contains(script)) {
-        document.body.removeChild(script);
-      }
-    };
-  }, []);
+//     return () => {
+//       if (document.body.contains(script)) {
+//         document.body.removeChild(script);
+//       }
+//     };
+//   }, []);
 
-  return null;
-};
+//   return null;
+// };
 
-export default PopAd;
+// export default PopAd;
