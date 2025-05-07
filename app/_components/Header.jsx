650277c21dbe7b4c21f0e5f5eac8f683374fe301
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2025-05-07 16:27:57 +0700

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,33 +4,30 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react"
 
 function Header() {
   const { user, isSignedIn } = useUser();
 
   return (
-    <div className="p-5 flex justify-between items-center border shadow-sm z-50 relative bg-white">
-      <div className="flex flex-row items-center gap-2">
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row items-center">
         <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
         <span className="text-blue-800 font-bold text-xl">Kosfunds</span>
       </div>
-
       {isSignedIn ? (
-        <div className="flex gap-3 items-center z-50 relative">
+        <div className="flex gap-3 items-center">
           <Link href={"/dashboard"}>
-            <Button variant="outline" className="rounded-full pointer-events-auto">
+            <Button variant="outline" className="rounded-full z-50 relative">
               Dashboard
             </Button>
           </Link>
           <UserButton />
         </div>
       ) : (
-        <div className="flex gap-3 items-center z-50 relative">
+        <div className="flex gap-3 items-center">
           <Link href={"/sign-in"}>
-            <Button className="rounded-full pointer-events-auto">
-              Get Started
-            </Button>
+            <Button className="rounded-full z-50 relative">Get Started</Button>
           </Link>
         </div>
       )}
